c7f491edd8bdd102fa816517da989797339f78a1
Richard <richard.ricciard@gmail.com>
2024-05-28 18:46:32 -0300

    chatsimulation

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+mensagens = []
+
+nome = input("Nome: ")
+
+while True:
+
+    os.system("clear")
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m ["nome"], "-", m ["texto"])
+
+    print("___________________")
+
+    texto = input("Mensagem: ")
+    if texto == "fim":
+        break
+
+    mensagens.append({
+        "nome": nome,
+        "texto": texto
+    })
